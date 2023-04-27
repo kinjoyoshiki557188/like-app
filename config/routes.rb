bcf42d83b6778b701f6dd48ad87eedabd3389a81
kinjoyoshiki557188 <124559662+kinjoyoshiki557188@users.noreply.github.com>
2023-04-27 12:57:25 +0900

    ネストの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'tweets#index'
-  resources :tweets, only: [:index, :new, :create]
+  resources :tweets, only: [:index, :new, :create] do
   resources :users, only: [:show]
+  end
 end
