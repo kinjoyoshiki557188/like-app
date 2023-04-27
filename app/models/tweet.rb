78e1f019bc8c86b41e9b7d3e5b441acb3dc0aeee
kinjoyoshiki557188 <124559662+kinjoyoshiki557188@users.noreply.github.com>
2023-04-27 13:06:03 +0900

    いいねボタンの表示設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,8 @@ class Tweet < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
   has_many :likes
+
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
 end
