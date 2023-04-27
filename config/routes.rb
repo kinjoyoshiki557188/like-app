43be92e33e1844d1d336bc19a8e6bc8025f56818
kinjoyoshiki557188 <124559662+kinjoyoshiki557188@users.noreply.github.com>
2023-04-27 13:03:11 +0900

    ネストの記述修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tweets#index'
   resources :tweets, only: [:index, :new, :create] do
-  resources :users, only: [:show]
+    resource :likes, only: [:create, :destroy]
   end
+  resources :users, only: :show
 end
