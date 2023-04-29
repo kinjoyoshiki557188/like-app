e69a363e8affdd4d0c1eda4f57771602d2e32db6
kinjoyoshiki557188 <124559662+kinjoyoshiki557188@users.noreply.github.com>
2023-04-29 16:09:08 +0900

    ボタンを押すとリロードせず見た目が変わるよう設定

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,14 +3,20 @@ class LikesController < ApplicationController
 
   def create
     like = current_user.likes.build(tweet_id: params[:tweet_id])
-    like.save
-    render 'create.js.erb'
+    if like.save
+      render 'create.js.erb'
+    else
+      # 保存に失敗した場合の処理
+    end
   end
 
   def destroy
     like = Like.find_by(tweet_id: params[:tweet_id], user_id: current_user.id)
-    like.destroy
-    render 'destroy.js.erb'
+    if like.destroy
+      render 'destroy.js.erb'
+    else
+      # 削除に失敗した場合の処理
+    end
   end
 
   def set_tweet
